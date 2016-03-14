7190a41748ca1f2585e567bc1330184db80d2c7a
jrburke <jrburke@gmail.com>
2016-03-14 16:42:59 -0700

    init

---
+++ libs/test.js
@@ -0,0 +1,2 @@
+document.getElementById('output').textContent =
+                           'swdn/libs/test.js file, not service worker cache';
---
+++ service-worker.js
@@ -0,0 +1,47 @@
+var cacheName = 'swdn-v1';
+
+var urlsToCache = [
+  '/swdn/libs/test.js',
+  '/swdn/test.html'
+];
+
+self.addEventListener('install', function(event) {
+  console.log(cacheName + '-install');
+  // Perform install steps
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  var url = event.request.url;
+  console.log(cacheName + '-fetch: ' + url);
+
+  if (url.indexOf('test.js') !== -1) {
+    event.respondWith(new Response(
+      "document.getElementById('output').textContent = " +
+      "'swdn/libs/test.js from service worker cache. GOOD';",
+      {
+        headers: { 'Content-Type': 'application/javascript' }
+      }
+    ));
+  } else {
+    console.log('Trying cache');
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+
+          return fetch(event.request);
+        }
+      )
+    );
+  }
+});
